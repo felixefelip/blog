dee3ab6e2064947799f01cf928cae475ade14fdb
Felipe <felippe.felix98@gmail.com>
2021-10-04 19:31:02 -0300

    Adição de informações de envio de email para produção

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,16 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: "felipe.testes98@gmail.com",
+    password: "Felipe@2021",
+    authentication: 'plain',
+    enable_starttls_auto: true
+  } 
+  
 end
